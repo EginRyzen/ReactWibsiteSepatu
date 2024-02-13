478e139638277aacc0b025aadf65dbb584503904
EginRyzen <muhammadaliagil@gmail.com>
2024-02-13 22:13:47 +0700

    top-product

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,45 @@ export default function App() {
       <div className="main">
         <About />
         <Product />
+        <div className="container">
+          <div id="top_product">
+            <h2>Top Product</h2>
+            <div className="box_top">
+              <div className="box_view">
+                <div className="card_product">
+                  <img src="./img/top_1.png" alt="" />
+                </div>
+                <div className="card_product">
+                  <img src="./img/top_2.png" alt="" />
+                </div>
+                <div className="card_product">
+                  <img src="./img/top_3.png" alt="" />
+                </div>
+                <div className="card_product">
+                  <img src="./img/top_4.png" alt="" />
+                </div>
+                <div className="card_product">
+                  <img src="./img/top_5.png" alt="" />
+                </div>
+              </div>
+              <div className="box_preview">
+                <img src="./img/top_1.png" alt="" />
+              </div>
+              <div className="box_deskripsi">
+                <div className="box_text">
+                  <h3>Nike P-6000</h3>
+                  <h4>The Nike P-6000 draws on the 2006 Nike Air Pegasus, bringing you a mash-up of iconic style thats breathable, comfortable and evocative of that early-2000s vibe.</h4>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, sequi! Nostrum debitis ad a corporis perferendis sapiente minus fuga autem laboriosam atque consequatur, odit sequi ipsam reiciendis labore? Quibusdam nulla
+                    nisi maxime incidunt alias. Velit cum alias, illum voluptas aliquam quisquam delectus reiciendis reprehenderit iste. Minus suscipit id vitae enim accusamus voluptates sint rem maxime, dolores dicta quibusdam officiis
+                    porro ducimus harum ipsam incidunt nostrum quos voluptate? Saepe, itaque tempore. Ab aliquam aperiam officia doloribus tenetur nam adipisci commodi accusamus animi facere, obcaecati natus consequatur maxime voluptas
+                    eveniet odit vero corrupti libero unde quisquam sit iste recusandae cupiditate incidunt! Fuga?
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
