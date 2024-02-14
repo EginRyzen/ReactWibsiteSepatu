090ce9e03fbec25637198101c47b5bc15d1ff6d8
EginRyzen <muhammadaliagil@gmail.com>
2024-02-14 21:02:51 +0700

    filter done

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,95 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFacebook } from "@fortawesome/free-brands-svg-icons";
 import { faInstagram } from "@fortawesome/free-brands-svg-icons";
 import { faTiktok } from "@fortawesome/free-brands-svg-icons";
+import { useState } from "react";
+
+const Card = [
+  {
+    id: 1,
+    img: "./img/product_1.jpg",
+    judul: "Adidas Run 80s Mens Sneakers - Bronze Strata",
+    harga: "Rp. 1.1000.00",
+  },
+  {
+    id: 2,
+    img: "./img/product_2.jpg",
+    judul: "Adidas Run 80s Mens Sneakers - Bronze Strata",
+    harga: "Rp. 1.1000.00",
+  },
+  {
+    id: 3,
+    img: "./img/product_3.jpg",
+    judul: "Adidas Run 80s Mens Sneakers - Bronze Strata",
+    harga: "Rp. 1.1000.00",
+  },
+  {
+    id: 4,
+    img: "./img/product_4.jpg",
+    judul: "Adidas Run 80s Mens Sneakers - Bronze Strata",
+    harga: "Rp. 1.1000.00",
+  },
+  {
+    id: 5,
+    img: "./img/product_5.jpg",
+    judul: "Adidas Run 80s Mens Sneakers - Bronze Strata",
+    harga: "Rp. 1.1000.00",
+  },
+  {
+    id: 6,
+    img: "./img/product_6.jpg",
+    judul: "Adidas Run 80s Mens Sneakers - Bronze Strata",
+    harga: "Rp. 1.1000.00",
+  },
+  {
+    id: 7,
+    img: "./img/product_7.jpg",
+    judul: "Adidas Run 80s Mens Sneakers - Bronze Strata",
+    harga: "Rp. 1.1000.00",
+  },
+  {
+    id: 8,
+    img: "./img/product_8.jpg",
+    judul: "Adidas Run 80s Mens Sneakers - Bronze Strata",
+    harga: "Rp. 1.1000.00",
+  },
+];
+
+const TopProduct = [
+  {
+    id: "1",
+    img: "./img/top_1.png",
+  },
+  {
+    id: "2",
+    img: "./img/top_2.png",
+  },
+  {
+    id: "3",
+    img: "./img/top_3.png",
+  },
+  {
+    id: "4",
+    img: "./img/top_4.png",
+  },
+  {
+    id: "5",
+    img: "./img/top_5.png",
+  },
+  {
+    id: "6",
+    img: "./img/top_6.png",
+  },
+];
 
 export default function App() {
+  const [topProduct] = useState(TopProduct);
+  const [selectedProduct, setSelectProduct] = useState(topProduct[0]);
+
+  function handleSelectProduct(id) {
+    const newProduct = topProduct.filter((Product) => Product.id === id);
+    setSelectProduct(newProduct[0]);
+  }
+
   return (
     <>
       <div className="main-header">
@@ -15,34 +102,13 @@ export default function App() {
       </div>
       <div className="main">
         <About />
-        <Product />
+        <Product Card={Card} />
         <div className="container">
           <div id="top_product">
             <h2>Top Product</h2>
             <div className="box_top">
-              <div className="box_view">
-                <div className="card_product">
-                  <img src="./img/top_1.png" alt="" />
-                </div>
-                <div className="card_product">
-                  <img src="./img/top_2.png" alt="" />
-                </div>
-                <div className="card_product">
-                  <img src="./img/top_3.png" alt="" />
-                </div>
-                <div className="card_product">
-                  <img src="./img/top_4.png" alt="" />
-                </div>
-                <div className="card_product">
-                  <img src="./img/top_5.png" alt="" />
-                </div>
-                <div className="card_product">
-                  <img src="./img/top_6.png" alt="" />
-                </div>
-              </div>
-              <div className="box_preview">
-                <img src="./img/top_1.png" alt="" />
-              </div>
+              <BoxView topProduct={topProduct} onSelectedProduct={handleSelectProduct} />
+              <BoxPreview selectedProduct={selectedProduct} />
               <div className="box_deskripsi">
                 <div className="box_text">
                   <h3>Nike P-6000</h3>
@@ -100,5 +166,25 @@ export default function App() {
         </footer>
       </div>
     </>
+  );
+}
+
+function BoxView({ topProduct, onSelectedProduct }) {
+  return (
+    <div className="box_view">
+      {topProduct?.map((data) => (
+        <div className="card_product" key={data.id} onClick={() => onSelectedProduct(data.id)}>
+          <img src={data.img} alt="" />
+        </div>
+      ))}
+    </div>
+  );
+}
+
+function BoxPreview({ selectedProduct }) {
+  return (
+    <div className="box_preview">
+      <img src={selectedProduct.img} alt="" />
+    </div>
   );
 }
--- src/Components/Product/Product.jsx
+++ src/Components/Product/Product.jsx
@@ -1,59 +1,9 @@
+/* eslint-disable react/prop-types */
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart } from "@fortawesome/free-solid-svg-icons";
 import { useState } from "react";
 
-const Card = [
-  {
-    id: 1,
-    img: "./img/product_1.jpg",
-    judul: "Adidas Run 80s Mens Sneakers - Bronze Strata",
-    harga: "Rp. 1.1000.00",
-  },
-  {
-    id: 2,
-    img: "./img/product_2.jpg",
-    judul: "Adidas Run 80s Mens Sneakers - Bronze Strata",
-    harga: "Rp. 1.1000.00",
-  },
-  {
-    id: 3,
-    img: "./img/product_3.jpg",
-    judul: "Adidas Run 80s Mens Sneakers - Bronze Strata",
-    harga: "Rp. 1.1000.00",
-  },
-  {
-    id: 4,
-    img: "./img/product_4.jpg",
-    judul: "Adidas Run 80s Mens Sneakers - Bronze Strata",
-    harga: "Rp. 1.1000.00",
-  },
-  {
-    id: 5,
-    img: "./img/product_5.jpg",
-    judul: "Adidas Run 80s Mens Sneakers - Bronze Strata",
-    harga: "Rp. 1.1000.00",
-  },
-  {
-    id: 6,
-    img: "./img/product_6.jpg",
-    judul: "Adidas Run 80s Mens Sneakers - Bronze Strata",
-    harga: "Rp. 1.1000.00",
-  },
-  {
-    id: 7,
-    img: "./img/product_7.jpg",
-    judul: "Adidas Run 80s Mens Sneakers - Bronze Strata",
-    harga: "Rp. 1.1000.00",
-  },
-  {
-    id: 8,
-    img: "./img/product_8.jpg",
-    judul: "Adidas Run 80s Mens Sneakers - Bronze Strata",
-    harga: "Rp. 1.1000.00",
-  },
-];
-
-export default function Product() {
+export default function Product({ Card }) {
   const [heartStatus, setHeartStatus] = useState({});
 
   const handleHeartClick = (id) => {
